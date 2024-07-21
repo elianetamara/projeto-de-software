56af1431afc6ebb7a6ddfe8943dabcd0b1e9cb0a
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2024-07-21 12:38:31 -0300

    refactor: alterando endereco

--- atv-1/v2-psoft-atv1/src/main/java/entities/Endereco.java
+++ atv-1/v2-psoft-atv1/src/main/java/entities/Endereco.java
@@ -2,12 +2,31 @@ package entities;
 
 public class Endereco {
 
+    private String id;
     private String rua;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
     private String cep;
 
-    public Endereco(String rua, String cep) {
+    public Endereco(String rua, String numero, String bairro, String cidade, String estado, String cep) {
         this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
         this.cep = cep;
+        this.setId();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId() {
+        String id = numero + bairro + cidade;
+        this.id = id;
     }
 
     public String getRua() {
@@ -16,6 +35,38 @@ public class Endereco {
 
     public void setRua(String rua) {
         this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 
     public String getCep() {
